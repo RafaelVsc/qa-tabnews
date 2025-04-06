3afaf8d9bad4ade365693bb0e8ad789d20c17368
Rafael Vescio <rafaelvsc88@gmail.com>
2025-04-06 14:43:54 +0000

    add style

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,35 @@ function Home() {
       textAlign: 'center',
       fontFamily: 'Arial, sans-serif',
       color: '#FFFFFF', // Text color
-      backgroundColor: '#511fb6', // Optional: Add a background color
+      background: 'linear-gradient(150deg, #511fb6, #Fa7e0c)',
+      animation: 'zoomGradient 7s infinite',
+      backgroundSize: '100% 100%',
     }}>
-      <h1>👋 Olá, eu sou o Rafael! 👨‍💻 </h1>
+      <style>
+        {`
+          @keyframes zoomGradient {
+            0% {
+              background-size: 100% 100%;
+            }
+            50% {
+              background-size: 200% 200%;
+            }
+            100% {
+              background-size: 100% 100%;
+            }
+          }
+        `}
+      </style>
+      <h1>👋 Olá, eu sou o Rafael! 👨‍💻</h1>
       <p>🚀 Falo sobre Qualidade de Software e Desenvolvimento 💡</p>
-      <a 
-        href="https://www.youtube.com/@QAverso" 
-        target="_blank" 
+      <a
+        href="https://www.youtube.com/@QAverso"
+        target="_blank"
         rel="noopener noreferrer"
         style={{
           marginTop: '20px',
           padding: '10px 20px',
-          backgroundColor: '#FA7e0c',
+          backgroundColor: '#Fa7e0c',
           color: '#FFFFFF',
           textDecoration: 'none',
           borderRadius: '5px',
