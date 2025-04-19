5616d132b0f0f4afd30e4bd5f349a7db2d9f156b
Rafael Vescio <rafaelvsc88@gmail.com>
2025-04-19 22:15:16 +0000

    run lint fix e add .prettierignore

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 function Home() {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      textAlign: 'center',
-      fontFamily: 'Arial, sans-serif',
-      color: '#FFFFFF', // Text color
-      background: 'linear-gradient(150deg, #511fb6, #Fa7e0c)',
-      animation: 'zoomGradient 7s infinite',
-      backgroundSize: '100% 100%',
-    }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        textAlign: "center",
+        fontFamily: "Arial, sans-serif",
+        color: "#FFFFFF", // Text color
+        background: "linear-gradient(150deg, #511fb6, #Fa7e0c)",
+        animation: "zoomGradient 7s infinite",
+        backgroundSize: "100% 100%",
+      }}
+    >
       <style>
         {`
           @keyframes zoomGradient {
@@ -29,19 +31,22 @@ function Home() {
         `}
       </style>
       <h1>👋 Olá, eu sou o Rafael! 👨‍💻</h1>
-      <p>🚀 Falo sobre Qualidade de Software, Automação de testes e Desenvolvimento! 💡</p>
+      <p>
+        🚀 Falo sobre Qualidade de Software, Automação de testes e
+        Desenvolvimento! 💡
+      </p>
       <a
         href="https://www.youtube.com/@QAverso"
         target="_blank"
         rel="noopener noreferrer"
         style={{
-          marginTop: '20px',
-          padding: '10px 20px',
-          backgroundColor: '#Fa7e0c',
-          color: '#FFFFFF',
-          textDecoration: 'none',
-          borderRadius: '5px',
-          fontWeight: 'bold'
+          marginTop: "20px",
+          padding: "10px 20px",
+          backgroundColor: "#Fa7e0c",
+          color: "#FFFFFF",
+          textDecoration: "none",
+          borderRadius: "5px",
+          fontWeight: "bold",
         }}
       >
         ▶️ Visite meu canal no YouTube
